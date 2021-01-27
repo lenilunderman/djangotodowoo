6caa02712be2982d0bc2825e25e437102a2f7ee0
Leni Lunderman <lenilunderman@gmail.com>
2021-01-26 22:18:21 -0500

    Changing the debug

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'rfppzu7jdny)#-4v)7%fyi(^!=*js53-$=z!zg3h-l2%8(9dry'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
